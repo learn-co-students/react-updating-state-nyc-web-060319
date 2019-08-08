043f94b12ef091ae86d7fa99ce3605cf2251d25e
TimeSmash <TimeSmash@github.com>
2019-08-08 15:27:54 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ClickityClick from './components/ClickityClick';
 
 ReactDOM.render(
-  <div>
-    Mount Components Here
-  </div>,
+    <ClickityClick />,
   document.getElementById('root')
-);
+); 
+
+//this.setState merges the contents it is given 
+//with the current state.
+//It is very similar to Object.assign({}, setStateComponents...)
